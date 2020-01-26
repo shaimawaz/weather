cd7e0715f1042a7e167f36972a8cc9c3512537f8
shaimawaz <eng.shaima.wazwaz@gmail.com>
2020-01-26 11:39:29 +0200

    header added but with no functionality of shrinking

--- src/app/header.directive.ts
+++ src/app/header.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHeader]'
 })
 export class HeaderDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  @HostListener('window:scroll') onWindowScroll() {
+    this.resize();
+  }
+  private resize() {
+    this.el.nativeElement.style.backgroundColor = 'red';
+  }
 }
